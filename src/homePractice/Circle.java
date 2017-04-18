8bd6406eafd6d93ba1f3c7e5d9f2cb2e7d3a451c
ruhul005 <ruhulamin@Ruhuls-MacBook-Pro.local>
2017-04-18 10:34:42 +0600

    Abstraction

---
+++ src/homePractice/Circle.java
@@ -0,0 +1,17 @@
+package homePractice;
+
+public class Circle extends GeometricObject{
+
+	@Override
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getPerimeter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
